273b11c6be4386784a010c24422c2e528cdf4e67
sonmu <sonmu.def@gmail.com>
2025-06-12 20:07:17 +0800

    install add

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -128,5 +128,18 @@ return {
     -- Enable theme
     require('onedark').load()
   end
+},
+{
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html", -- if you have `nvim-treesitter` installed
+    dependencies = {
+        "nvim-telescope/telescope.nvim",
+        -- "ibhagwan/fzf-lua",
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+    },
+    opts = {
+        -- configuration goes here
+    },
 }
 }
