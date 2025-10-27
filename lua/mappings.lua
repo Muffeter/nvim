6194d206d91d7f61c84724475ff2d25998e6c851
sonmu <sonmu.def@gmail.com>
2025-10-27 15:15:37 +0800

    telescope live_grep add

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,39 +3,33 @@ vim.g.mapleader = "<Space>"
 
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("n", "<C-o>", "m`o<ESC>``")
 
--- move 
+-- move
 map("n", "<C-h>", "^", { desc = "Move cursor beginning non char" })
-map("n", "<C-l>", "$", { desc = "Move cursor end" } )
-map("i", "<C-h>", "<Left>", { desc = "Move cursor Left" } )
-map("i", "<C-l>", "<Right>", { desc = "Move cursor Right" } )
-map("i", "<C-j>", "<Down>", { desc = "Move cursor Down" } )
-map("i", "<C-k>", "<Up>", { desc = "Move cursor Up" } )
+map("n", "<C-l>", "$", { desc = "Move cursor end" })
+map("i", "<C-h>", "<Left>", { desc = "Move cursor Left" })
+map("i", "<C-l>", "<Right>", { desc = "Move cursor Right" })
+map("i", "<C-j>", "<Down>", { desc = "Move cursor Down" })
+map("i", "<C-k>", "<Up>", { desc = "Move cursor Up" })
 
--- For nvim-tree
-map("n", "<C-\\>", ":NvimTreeToggle<CR>")
+-- modify
+map("n", "dw", '"1dw', { desc = "delete word without copy into latest register" })
+map("n", "dd", '"1dd', { desc = "delete line without copy into latest register" })
+map("n", "cc", '"1cc', { desc = "cut word without copy into latest register" })
+map("n", "cw", '"1cw', { desc = "cut line without copy into latest register" })
+map("n", "<C-o>", "m`o<ESC>``", { desc = "new a line without going into insert mode" })
+
+-- nvim-tree
+map("n", "<C-\\>", ":NvimTreeToggle<CR>", { desc = "Toggle the file explorer" })
 
 -- lspconfig
 map("n", "<C-k>", vim.lsp.buf.hover)
 map("n", "<A-S-f>", vim.lsp.buf.format)
 
-
-
+-- telescope
+local builtin = require("telescope.builtin")
+map("n", "<C-f>", builtin.live_grep, { desc = "find text across project" })
 
 map("i", "jk", "<ESC>")
-map('i', '<C-o>', 'copilot#Accept("\\<CR>")', {
-  expr = true,
-  replace_keycodes = false
-})
-map("i", '<C-w>', '<Plug>(copilot-suggest)')
-
 map("t", "<c-space>", "<C-\\><C-n>")
--- map("i", "<C-j>", "<Down>")
--- map("i", "<C-k>", "<Up>")
 vim.g.copilot_no_tab_map = true
-
-
-
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-print "hi"
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -218,5 +218,5 @@ return {
 				desc = "Quickfix List (Trouble)",
 			},
 		},
-	}
+	},
 }
