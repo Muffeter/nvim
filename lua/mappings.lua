ff9035c9133ac6713c5628af4ad3b16750556108
guiming <1317808323@qq.com>
2026-01-14 11:10:54 +0800

    truncate too long path when telescop

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -40,7 +40,10 @@ map("i", "<A-S-f>", conform.format)
 -- telescope
 local builtin = require("telescope.builtin")
 map("n", "<C-f>", builtin.live_grep, { desc = "find text across project" })
-map("n", "<C-s>", builtin.git_files, { desc = "find text across project" })
+map("n", "<C-s>",
+  function ()
+    builtin.git_files{path_display = { "truncate" }}
+  end, { desc = "find text across project" })
 
 map("i", "jk", "<ESC>")
 map("t", "<c-space>", "<C-\\><C-n>")
