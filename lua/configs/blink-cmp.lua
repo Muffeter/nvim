e5fbaa47a575863481fcda2fc88c3636caf7dec5
guiming <1317808323@qq.com>
2026-01-19 18:41:48 +0800

    blink-cmp add lazydev

--- lua/configs/blink-cmp.lua
+++ lua/configs/blink-cmp.lua
@@ -4,13 +4,23 @@ return {
   version = '1.*',
   dependencies = { 'rafamadriz/friendly-snippets' },
   opts = {
-    keymap = { 
-    preset = 'super-tab',
-  },
-    appearance = { nerd_font_variant = 'mono' },
-    completion = { documentation = { auto_show = false } },
-    sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
-    fuzzy = { implementation = 'lua' },
-  },
-}
+      keymap = { 
+        preset = 'super-tab',
+      },
+      appearance = { nerd_font_variant = 'mono' },
+      completion = { documentation = { auto_show = false } },
+      sources = { 
+        default = { "lazydev", 'lsp', 'path', 'snippets', 'buffer' },
+
+        providers = {
+          lazydev = {
+              name = "LazyDev",
+              module = "lazydev.integrations.blink",
+              score_offset = 100,
+          }
+        }
+      },
+      fuzzy = { implementation = 'prefer_rust_with_warning' },
+    },
+  }
 }
