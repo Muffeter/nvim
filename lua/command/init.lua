81bbe20a5c9ff04acb28f64c63d49044fbf08f6f
sonmu <sonmu.def@gmail.com>
2026-01-30 20:23:44 +0800

    update cmd

--- lua/command/init.lua
+++ lua/command/init.lua
@@ -7,6 +7,9 @@ end, {})
 
 -- Close all buffers but the current one
 cmd("ClearBuffer", "%bd|e#", {})
+-- Update the config
+local configPaht = vim.fn.stdpath('config')
+cmd("UpdateBygit", "!git -C " .. configPaht .. " pull", {})
 
 
 -- ref: https://neovim.io/doc/user/lua-guide.html#lua-guide-commands-create
